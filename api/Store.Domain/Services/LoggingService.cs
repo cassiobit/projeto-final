54242a025217b6f855b8543129976946e39fbad9
Cassio Thiesen Bitelo <cassio.bitelo@cwi.com.br>
2022-06-06 23:05:35 -0300

    feat(Customer): logging improvements

--- api/Store.Domain/Services/LoggingService.cs
+++ api/Store.Domain/Services/LoggingService.cs
@@ -9,38 +9,23 @@ namespace Store.Domain.Services
         private readonly ILogger<LoggingService> _logger;
 
         private int _requestNumber;
-        private double _totalPauseDuration;
 
         public LoggingService(ILogger<LoggingService> logger)
         {
             _logger = logger;
             _requestNumber = 0;
-            _totalPauseDuration = 0;
         }
 
 
         public void LogGCInfo()
         {
             var gcInfo = GC.GetGCMemoryInfo();
-            _totalPauseDuration = GetTotalPauseDuration(gcInfo);
-            long pinnedObjects = gcInfo.PinnedObjectsCount;
-            long totalCommittedBytes = gcInfo.TotalCommittedBytes;
+            double totalPauseDuration = gcInfo.PauseDurations[0].TotalMilliseconds + gcInfo.PauseDurations[1].TotalMilliseconds;
             long finalizationPendingCount = gcInfo.FinalizationPendingCount;
             long totalManagedMemory = GC.GetTotalMemory(false);
 
-            _logger.LogInformation("#{0} - PauseDuration(ms): {1} - ManagedMemory(bytes): {2} - TotalCommittedBytes: {3} - FinalizationPendingCount: {4} - Gen 0: {5} - Gen 1: {6} - Gen 2: {7}", 
-              ++_requestNumber, _totalPauseDuration, totalManagedMemory, totalCommittedBytes, finalizationPendingCount, GC.CollectionCount(0), GC.CollectionCount(1), GC.CollectionCount(2));
-        }
-
-        private double GetTotalPauseDuration(GCMemoryInfo gcInfo){
-            var total = gcInfo.PauseDurations[0].TotalMilliseconds + gcInfo.PauseDurations[1].TotalMilliseconds;
-
-            if(total == 0 && _totalPauseDuration == 0)
-                return 0;
-            else if(total == _totalPauseDuration)
-                return _totalPauseDuration;
-            else
-                return _totalPauseDuration + total;
+            _logger.LogInformation("#{0} - PauseDuration(ms): {1} - ManagedMemory(bytes): {2} - FinalizationPendingCount: {3} - Gen 0: {4} - Gen 1: {5} - Gen 2: {6}", 
+              ++_requestNumber, totalPauseDuration, totalManagedMemory, finalizationPendingCount, GC.CollectionCount(0), GC.CollectionCount(1), GC.CollectionCount(2));
         }
     }
 }
